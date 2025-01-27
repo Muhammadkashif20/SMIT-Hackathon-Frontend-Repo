b0bd57b9004ee57a8ccbbaeb7a4f5d5fe9c2d40a
Muhammad Kashif <154686104+Muhammadkashif20@users.noreply.github.com>
2025-01-27 18:14:20 +0500

    Add files via upload
    vercel err solve

---
+++ src/Home.jsx
@@ -0,0 +1,23 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import LoanCalculator from "../components/LoanCalculator";
+
+function Home() {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Loan Calculator */}
+      <LoanCalculator />
+
+      {/* Footer */}
+      <Footer />
+    </>
+  );
+}
+export default Home;
